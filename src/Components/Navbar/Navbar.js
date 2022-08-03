003ba4db395186642c8672ec1b30f644cabeb113
AkasHZamaN <akashzaman4@gmail.com>
2022-08-03 17:47:53 +0600

    modified

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -13,6 +13,9 @@ const Navbar = () => {
     <div>
       <div className="navbar bg-neutral">
         <div className="flex-1">
+        <div className="text-white">
+            <Link to={'/'}>Home</Link>
+            </div>
           <div className="dropdown">
             <label onClick={filterItems} tabIndex="2" className="btn m-1">
               Category
@@ -35,30 +38,7 @@ const Navbar = () => {
               className="input input-bordered"
             />
           </div>
-          <div className="dropdown dropdown-end">
-            <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img src="https://placeimg.com/80/80/people" alt="" />
-              </div>
-            </label>
-            <ul
-              tabIndex="0"
-              className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <Link to={"#"} className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </Link>
-              </li>
-              <li>
-                <Link to={"#"}>Settings</Link>
-              </li>
-              <li>
-                <Link to={"#"}>Logout</Link>
-              </li>
-            </ul>
-          </div>
+          <button class="btn btn-outline btn-accent">Search</button>
         </div>
       </div>
     </div>
